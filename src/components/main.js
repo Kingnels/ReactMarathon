78986f5dfaf8040243bce74222cbb9f073ace17a
Kingnels <ifeanyiibe2022@gmail.com>
2022-10-04 16:15:55 +0100

    finished business card

--- src/components/main.js
+++ src/components/main.js
@@ -8,7 +8,7 @@ function Main(){
         I am a frontend developer with a particular interest in making things simple and automating daily tasks. I try to keep up with security and best practices, and am always looking for new things to learn.
         </p>
 
-        <h2>Interests</h2>
+        <h2 className='two'>Interests</h2>
         <p>
         Food expert. Music scholar. Reader. Internet fanatic. Bacon buff. Entrepreneur. Travel geek. Pop culture ninja. Coffee fanatic.
         </p>
